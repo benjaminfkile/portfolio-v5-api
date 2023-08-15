67b60985d5fda0fb52be81081fc862a8bb96e37d
Ben Kile <ben.f.kile@elmutility.com>
2023-08-15 16:44:28 -0600

    change video_name to file_name

--- src/types/portfolioItemTypes.ts
+++ src/types/portfolioItemTypes.ts
@@ -3,8 +3,9 @@ type PortfolioItemTypes = {
     title: string
     intro: string
     description: string
-    video_name: string
+    file_name: string
     order: number
     url: string | null
     repo: string | null
+    media_type: number
 }
