b918245ec60360893eb28cae39175b3df3fb5339
Ben Kile <ben.f.kile@elmutility.com>
2023-08-15 09:46:58 -0600

    using smart stream

--- src/routers/MediaRouter.tsx
+++ src/routers/MediaRouter.tsx
@@ -1,20 +1,17 @@
 import express, { Request, Response } from "express"
+import createAWSStream from "../streamClient"
 const mediaRouter = express.Router()
-const mediaService = require("../services/MediaService")
 const jsonParser = express.json()
 
 mediaRouter
     .route("/")
     .get(jsonParser, async (req: Request, res: Response) => {
         try {
-            const s3 = req.app.get("s3")
-            const io = req.app.get("io")
-            const socketId = req.query.socketId
-            const key = req.query.key
-            mediaService.streamChunks(s3, io, socketId, key, null)
-            res.status(200).send({ message: "streaming has started", err: null })
+            const key: any = req.query.key
+            const stream = await createAWSStream(key)
+            stream.pipe(res)            
         } catch (err) {
-            res.status(200).send({ message: "streaming error", err: err })
+            res.status(200).send({err: err})
         }
     })
 module.exports = mediaRouter
