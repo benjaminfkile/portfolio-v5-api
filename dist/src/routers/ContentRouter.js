d43caef5a86bb7877f30e4f247b679f671118c06
Ben Kile <ben.f.kile@elmutility.com>
2024-10-17 09:35:43 -0600

    use async/await instead of promise chaining in content router

--- dist/src/routers/ContentRouter.js
+++ dist/src/routers/ContentRouter.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -6,42 +15,22 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const contentRouter = express_1.default.Router();
 const contentService = require("../services/ContentService");
-contentRouter
-    .route("/")
-    .get((req, res) => {
+contentRouter.route("/").get((req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         let content = { about: "", portfolioItems: [], skillItems: [], timelineItems: [] };
         const db = req.app.get("db");
-        contentService.getAbout(db).then((about) => {
-            content.about = about.text;
-        }).then(() => {
-            contentService.getPortfolioItems(db)
-                .then((portfolioItems) => {
-                content.portfolioItems = portfolioItems;
-            }).then(() => {
-                contentService.getSkillItems(db)
-                    .then((skillItems) => {
-                    content.skillItems = skillItems;
-                }).then(() => {
-                    contentService.getTimelineItems(db)
-                        .then((timelineItems) => {
-                        content.timelineItems = timelineItems;
-                        res.send({ content: content, error: false, errorMsg: {} });
-                    }).catch((error) => {
-                        res.send({ content: null, error: true, errorMsg: error });
-                    });
-                }).catch((error) => {
-                    res.send({ content: null, error: true, errorMsg: error });
-                });
-            }).catch((error) => {
-                res.send({ content: null, error: true, errorMsg: error });
-            });
-        }).catch((error) => {
-            res.send({ content: null, error: true, errorMsg: error });
-        });
+        const about = yield contentService.getAbout(db);
+        content.about = about.text;
+        const portfolioItems = yield contentService.getPortfolioItems(db);
+        content.portfolioItems = portfolioItems;
+        const skillItems = yield contentService.getSkillItems(db);
+        content.skillItems = skillItems;
+        const timelineItems = yield contentService.getTimelineItems(db);
+        content.timelineItems = timelineItems;
+        res.send({ content, error: false, errorMsg: {} });
     }
     catch (error) {
         res.send({ content: null, error: true, errorMsg: error });
     }
-});
+}));
 module.exports = contentRouter;
--- src/routers/contentRouter.tsx
+++ src/routers/contentRouter.tsx
@@ -2,41 +2,27 @@ import express, { Request, Response } from "express"
 const contentRouter = express.Router()
 const contentService = require("../services/ContentService")
 
-contentRouter
-    .route("/")
-    .get((req: Request, res: Response) => {
-        try {
-            let content: ContentResponseTypes = { about: "", portfolioItems: [], skillItems: [], timelineItems: [] }
-            const db = req.app.get("db")
-            contentService.getAbout(db).then((about: { id: number, text: string }) => {
-                content.about = about.text
-            }).then(() => {
-                contentService.getPortfolioItems(db)
-                    .then((portfolioItems: PortfolioItemTypes[]) => {
-                        content.portfolioItems = portfolioItems
-                    }).then(() => {
-                        contentService.getSkillItems(db)
-                            .then((skillItems: SkillItemTypes[]) => {
-                                content.skillItems = skillItems
-                            }).then(() => {
-                                contentService.getTimelineItems(db)
-                                    .then((timelineItems: TimelineItemTypes[]) => {
-                                        content.timelineItems = timelineItems
-                                        res.send({ content: content, error: false, errorMsg: {} })
-                                    }).catch((error: Error) => {
-                                        res.send({ content: null, error: true, errorMsg: error })
-                                    })
-                            }).catch((error: Error) => {
-                                res.send({ content: null, error: true, errorMsg: error })
-                            })
-                    }).catch((error: Error) => {
-                        res.send({ content: null, error: true, errorMsg: error })
-                    })
-            }).catch((error: Error) => {
-                res.send({ content: null, error: true, errorMsg: error })
-            })
-        } catch (error) {
-            res.send({ content: null, error: true, errorMsg: error })
-        }
-    })
+contentRouter.route("/").get(async (req: Request, res: Response) => {
+    try {
+        let content: ContentResponseTypes = { about: "", portfolioItems: [], skillItems: [], timelineItems: [] }
+        const db = req.app.get("db")
+
+        const about = await contentService.getAbout(db)
+        content.about = about.text
+
+        const portfolioItems = await contentService.getPortfolioItems(db)
+        content.portfolioItems = portfolioItems
+
+        const skillItems = await contentService.getSkillItems(db)
+        content.skillItems = skillItems
+
+        const timelineItems = await contentService.getTimelineItems(db)
+        content.timelineItems = timelineItems
+
+        res.send({ content, error: false, errorMsg: {} })
+    } catch (error) {
+        res.send({ content: null, error: true, errorMsg: error })
+    }
+})
+
 module.exports = contentRouter
