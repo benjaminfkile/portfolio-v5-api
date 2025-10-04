7427f0d6ebd00e8ff975f312dc30975afad36269
Ben Kile <benjaminfkile@gmail.com>
2025-10-04 17:01:26 -0500

    try fix yaml

--- src/routers/mediaRouter.ts
+++ src/routers/mediaRouter.ts
@@ -1,68 +1,74 @@
 import express from "express"
-// import * as AWS from "aws-sdk"
-//import mime from "mime-types"
+import { S3Client, HeadObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3"
+import mime from "mime-types"
+import { Readable } from "stream"
 
 const mediaRouter = express.Router()
-// const s3 = new AWS.S3({
-//   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-//   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-// })
+
+// v3 S3 client uses region and IAM role creds automatically
+const s3 = new S3Client({
+  region: process.env.AWS_REGION
+  // credentials are automatically picked up from IAM role in EC2/ECS,
+  // so you don’t need to hardcode accessKeyId/secretAccessKey
+})
 
 mediaRouter.get("/", async (req, res) => {
-  // const key = req.query.key as string
-  // const range = req.headers.range
-  // const contentType = mime.lookup(key) || "application/octet-stream"
+  const key = req.query.key as string
+  const range = req.headers.range
+  const contentType = mime.lookup(key) || "application/octet-stream"
 
-  // if (!key) return res.status(400).send("Missing key")
+  if (!key) return res.status(400).send("Missing key")
 
-  // const params: AWS.S3.GetObjectRequest = {
-  //   Bucket: process.env.S3_BUCKET_NAME!,
-  //   Key: key
-  // }
+  const bucket = process.env.S3_BUCKET_NAME!
+  const params = { Bucket: bucket, Key: key }
 
   try {
-    // const head = await s3.headObject(params).promise()
-    // const fileSize = head.ContentLength!
+    // Get object metadata (file size)
+    const head = await s3.send(new HeadObjectCommand(params))
+    const fileSize = head.ContentLength!
 
-    // if (range) {
-    //   const [startStr, endStr] = range.replace(/bytes=/, "").split("-")
-    //   const start = parseInt(startStr, 10)
-    //   const end = endStr ? parseInt(endStr, 10) : fileSize - 1
-    //   const chunkSize = end - start + 1
+    if (range) {
+      // Handle partial requests
+      const [startStr, endStr] = range.replace(/bytes=/, "").split("-")
+      const start = parseInt(startStr, 10)
+      const end = endStr ? parseInt(endStr, 10) : fileSize - 1
+      const chunkSize = end - start + 1
 
-    //   const stream = s3.getObject({
-    //     ...params,
-    //     Range: `bytes=${start}-${end}`
-    //   }).createReadStream()
+      const getObj = await s3.send(
+        new GetObjectCommand({
+          ...params,
+          Range: `bytes=${start}-${end}`
+        })
+      )
 
-    //   res.writeHead(206, {
-    //     "Content-Range": `bytes ${start}-${end}/${fileSize}`,
-    //     "Accept-Ranges": "bytes",
-    //     "Content-Length": chunkSize,
-    //     "Content-Type": contentType,
-    //     "Access-Control-Allow-Origin": "*",
-    //     "Cross-Origin-Resource-Policy": "cross-origin"
-    //   })
+      res.writeHead(206, {
+        "Content-Range": `bytes ${start}-${end}/${fileSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": chunkSize,
+        "Content-Type": contentType,
+        "Access-Control-Allow-Origin": "*",
+        "Cross-Origin-Resource-Policy": "cross-origin"
+      })
 
-    //   stream.pipe(res)
-    // } else {
-    //   const stream = s3.getObject(params).createReadStream()
+      ;(getObj.Body as Readable).pipe(res)
+    } else {
+      // Full file
+      const getObj = await s3.send(new GetObjectCommand(params))
 
-    //   res.writeHead(200, {
-    //     "Content-Length": fileSize,
-    //     "Content-Type": contentType,
-    //     "Accept-Ranges": "bytes",
-    //     "Access-Control-Allow-Origin": "*",
-    //     "Cross-Origin-Resource-Policy": "cross-origin"
-    //   })
+      res.writeHead(200, {
+        "Content-Length": fileSize,
+        "Content-Type": contentType,
+        "Accept-Ranges": "bytes",
+        "Access-Control-Allow-Origin": "*",
+        "Cross-Origin-Resource-Policy": "cross-origin"
+      })
 
-    //   stream.pipe(res)
-    // }
-    res.send("upgrading to aws sdk v3, come back later")
+      ;(getObj.Body as Readable).pipe(res)
+    }
   } catch (err) {
     console.error(err)
     res.status(500).send("Error streaming file")
   }
 })
 
-module.exports = mediaRouter
+export default mediaRouter
